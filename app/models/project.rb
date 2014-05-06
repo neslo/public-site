2b6567d4a9c6da0fc2577c681a940ae0d2249b9c
Chris D'Aloisio <chris@creativecontrol.com.au>
2014-05-06 22:18:53 +1000

    resolved model errors and added to activeadmin

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,11 @@
 class Project < ActiveRecord::Base
-  has_one :team, class_name: ProjectTeam
-  has_many :roles, class_name: ProjectRole, through: :team
+  has_one :team, class_name: 'ProjectTeam'
+  has_many :roles, through: :team
+
+  after_create :create_team
+
+  private
+  def create_team
+    self.create_team!
+  end
 end
--- app/models/project_role.rb
+++ app/models/project_role.rb
@@ -1,5 +1,8 @@
 class ProjectRole < ActiveRecord::Base
-  belongs_to :project_team
-  belongs_to :previous_person, class: Person
+  belongs_to :team, class_name: 'ProjectTeam', foreign_key: 'project_team_id'
+  belongs_to :previous_person, class_name: "Person"
   belongs_to :person
+  has_one :project, through: :team
+
+  validates_presence_of :team
 end
