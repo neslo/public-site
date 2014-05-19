d0f1699e9e477fab0498dd33c8866528b4d7a24c
Christopher Chow <chris@chowie.net>
2014-05-19 22:18:55 +1000

    Switch to official Twitter Bootstrap SASS port.
    Include the single helper that was being used from the
    twitter-bootstrap-rails gem in ApplicationHelper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  # From https://github.com/seyhunak/twitter-bootstrap-rails
+  def bootstrap_flash
+    flash_messages = []
+    flash.each do |type, message|
+      # Skip empty messages, e.g. for devise messages set to nothing in a locale file.
+      next if message.blank?
+
+      type = type.to_sym
+      type = :success if type.to_s == :notice.to_s
+      type = :error   if type.to_s == :alert.to_s
+      next unless [:error, :info, :success, :warning].include?(type)
+
+      Array(message).each do |msg|
+        text = content_tag(:div,
+                           content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") +
+                           msg, :class => "alert fade in alert-#{type}")
+        flash_messages << text if msg
+      end
+    end
+    flash_messages.join("\n").html_safe
+  end
 end
